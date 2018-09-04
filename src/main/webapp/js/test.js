59453b59b69d2fc2142a9d68361f9accec097f1b
pracct0 <42689286+pracct0@users.noreply.github.com>
2018-09-04 11:15:23 +0800

    Update test.js

--- src/main/webapp/js/test.js
+++ src/main/webapp/js/test.js
@@ -45,3 +45,29 @@ function validateUserId() {
    ajaxRequest.send(null); 
 }
 
+function setMessageUsingDOM(message) {
+    var userMessageElement = document.getElementById("userIdMessage");
+	
+    var messageText; 
+    
+	if (message == "false") { 
+       userMessageElement.style.color = "red"; 
+       messageText = "Invalid User Id"; 
+    } else { 
+       userMessageElement.style.color = "green"; 
+       messageText = "Valid User Id"; 
+    } 
+    var messageBody = document.createTextNode(messageText); 
+ 
+ 
+    // if the messageBody element has been created simple  
+    // replace it otherwise append the new element 
+ 
+    if (userMessageElement.childNodes[0]) { 
+       userMessageElement.replaceChild(messageBody, 
+       userMessageElement.childNodes[0]); 
+    } else { 
+       userMessageElement.appendChild(messageBody); 
+    } 
+}
+
