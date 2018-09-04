e92819a83aeaa0c280f6dbd50343a04c805b7a32
pracct0 <42689286+pracct0@users.noreply.github.com>
2018-09-04 11:06:44 +0800

    Update test.js

--- src/main/webapp/js/test.js
+++ src/main/webapp/js/test.js
@@ -1,1 +1,47 @@
 
+
+var ajaxRequest;
+
+function ajaxFunction(){
+ try{
+   // Opera 8.0+, Firefox, Safari 
+   ajaxRequest = new XMLHttpRequest(); 
+ }catch (e){
+   // Internet Explorer Browsers 
+   try{ 
+      ajaxRequest = new ActiveXObject("Msxml2.XMLHTTP"); 
+   }catch (e) { 
+      try{ 
+         ajaxRequest = new ActiveXObject("Microsoft.XMLHTTP"); 
+      }catch (e){ 
+         // Something went wrong 
+         alert("Your browser broke!"); 
+         return false; 
+      }
+   }
+ } 
+}
+
+function validateUserId() {
+  
+   ajaxFunction(); 
+   // Here processRequest() is the callback function. 
+   ajaxRequest.onreadystatechange = function() {
+
+	alert(ajaxRequest.readyState);
+	alert(ajaxRequest.status);
+	if (ajaxRequest.readyState == 4) {
+		
+         var message = "true";
+	  setMessageUsingDOM(message);
+	 }
+   }
+   
+   var userId = document.getElementById("userid").value; 
+   
+   var url = "http://kim-kim-project.a3c1.starter-us-west-1.openshiftapps.com/hello";
+  
+   ajaxRequest.open("GET", url, true); 
+   ajaxRequest.send(null); 
+}
+
