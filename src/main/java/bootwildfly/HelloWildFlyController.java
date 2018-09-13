f34ada35d025c15642d35ed773ecce9792302d03
pracct0 <42689286+pracct0@users.noreply.github.com>
2018-09-13 10:41:37 +0800

    Update HelloWildFlyController.java

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -2,6 +2,14 @@ package bootwildfly;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HelloWildFlyController {
@@ -11,4 +19,50 @@ public class HelloWildFlyController {
     public String sayHello(){
         return ("Hello, Magnus SpringBoot app on Wildfly, v1");
     }
+
+
+    @PostMapping("/api/search")
+    public ResponseEntity<?> getSearchResultViaAjax(
+            @Valid @RequestBody SearchCriteria search, Errors errors) {
+
+        AjaxResponseBody result = new AjaxResponseBody();
+
+        //If error, just return a 400 bad request, along with the error message
+        if (errors.hasErrors()) {
+
+            result.setMsg(errors.getAllErrors()
+                        .stream().map(x -> x.getDefaultMessage())
+                        .collect(Collectors.joining(",")));
+
+            return ResponseEntity.badRequest().body(result);
+
+        }
+
+		
+        List<User> users = userService.findByUserNameOrEmail(search.getUsername());
+		List<User> users = new ArrayList<User>();
+
+        User user1 = new User("mkyong", "password111", "mkyong@yahoo.com");
+        User user2 = new User("yflow", "password222", "yflow@yahoo.com");
+        User user3 = new User("laplap", "password333", "mkyong@yahoo.com");
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+		
+		users = users.stream()
+            .filter(x -> x.getUsername().equalsIgnoreCase(username))
+            .collect(Collectors.toList());
+		
+        if (users.isEmpty()) {
+            result.setMsg("no user found!");
+        } else {
+            result.setMsg("success");
+        }
+        result.setResult(users);
+
+        return ResponseEntity.ok(result);
+
+    }
+	
 }
